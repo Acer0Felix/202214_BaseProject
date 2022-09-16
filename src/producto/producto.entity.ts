bc3373a846e9828e5bfb0eb80d85b30caff5f14c
Félix Acero <acerofel@uniandes.edu.co>
2022-09-16 15:49:43 -0500

    Creación de entidad producto

---
+++ src/producto/producto.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ProductoEntity {
+    @PrimaryGeneratedColumn('uuid')
+        id: string;
+    
+    @Column()
+    nombre: string;
+
+    @Column()
+    precio: number;
+
+    @Column()
+    tipo: string;
+}
