8376a6781af028f2524e11285260039acf7e3caa
Félix Acero <acerofel@uniandes.edu.co>
2022-09-18 10:34:47 -0500

    adición de logia última  parte

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { ProductoModule } from './producto/producto.module';
 import { TiendaModule } from './tienda/tienda.module';
 import { ProductoTiendaModule } from './producto-tienda/producto-tienda.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TiendaEntity } from './tienda/tienda.entity';
+import { ProductoEntity } from './producto/producto.entity';
 
 @Module({
-  imports: [ProductoModule, TiendaModule, ProductoTiendaModule],
+  imports: [ProductoModule, TiendaModule, ProductoTiendaModule, 
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'Sol1DeoGlor1a',
+    database: 'tienda',
+    entities: [ProductoEntity, TiendaEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
